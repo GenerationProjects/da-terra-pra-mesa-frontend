dfb7ae4f46098a2dd4216d49511c7d10011fa543
Felipe Sousa <felipe.fps09@hotmail.com>
2023-06-19 11:43:24 -0300

    Criando rotas para páginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Cadastro from './pages/Cadastro'
+import Sobre from './pages/Sobre'
 import './App.css'
+
 
 export default function App() {
 
-
   return (
 
-    <></>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/cadastrar' element={<Cadastro />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/sobre' element={<Sobre/>}/>
+        </Routes>
+      </BrowserRouter>
+    </>
 
   )
 }
