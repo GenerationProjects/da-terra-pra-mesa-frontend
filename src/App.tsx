bb44be49d1f3f9c2909075a9a28e77551fb99fda
Felipe Sousa <felipe.fps09@hotmail.com>
2023-06-19 18:43:42 -0300

    Resolvendo conflitos

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,15 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 export default function App() {
 
+=======
+import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Sobre from './pages/Sobre'
+
+export default function App() {
+>>>>>>> f80bc6b (Criação das Rotas e página home)
   return (
     <>
     <BrowserRouter>
@@ -24,16 +33,33 @@ export default function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home />} />
+<<<<<<< HEAD
           <Route path='/cadastrar' element={<Cadastro />} />
           <Route path='/login' element={<Login />} />
+<<<<<<< HEAD
           <Route path='/sobre' element={<Sobre />} />
+=======
+          <Route path='/sobre' element={<Sobre/>}/>
+=======
+          <Route path='/home' element={<Home />}/>
+          <Route path='/login' element={<Login />}/>
+          <Route path='/sobre' element={<Sobre />}/>
+>>>>>>> f80bc6b (Criação das Rotas e página home)
+>>>>>>> 023b421 (Resolvendo conflitos)
         </Routes>
       </BrowserRouter>
     </>
   )
+<<<<<<< HEAD
 }
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 =======
 export default App
 >>>>>>> b07a8e3 (subindo navbar)
+=======
+=======
+}
+>>>>>>> f80bc6b (Criação das Rotas e página home)
+>>>>>>> 023b421 (Resolvendo conflitos)
