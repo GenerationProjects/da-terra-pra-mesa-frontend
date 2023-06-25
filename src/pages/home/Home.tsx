838c76e7fd7a6b598039113b333d3c79a2b83f27
Felipe Sousa <felipe.fps09@hotmail.com>
2023-06-24 22:29:42 -0300

    Finalizando pagina home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,27 +1,33 @@
-import { Box, Button, Grid, Typography } from "@material-ui/core";
+import { Box, Grid, Typography } from "@material-ui/core";
+import Arrow from '../../assets/img/Seta.svg'
 import FruitShop from '../../assets/img/home-img.svg'
 import './home.css'
 
 export default function Home() {
     return (
-        <><Box className="container">
-            <Grid container xs={12} direction="row">
-                <Box className="filho">
-                    <Grid item xs={12} sm={6} >
-                        <Typography className="texts">
-                            <h1>Da Terra Pra Mesa</h1>
-                            Conectamos você com agricultores sustentáveis e produtos de qualidade.
-                        </Typography>
-                        <Button  ></Button>
-                    </Grid >
-                </Box>
-                <Grid item xs={12} sm={6} justifyContent={`center`}>
-                    <Box className="box-img">
-                        <img src={FruitShop} alt="Mulher no lado esquedo de uma barraca que contem uma balança e legumes colhidos" className="img-container" />
+        <>
+            <Grid xs={12} className="box-container" >
+                <Grid item xs={6} className="grid-item">
+                    <Box className="box-title">
+                        <Typography variant="h4" className="title-h4">Da Terra Pra Mesa</Typography>
+                        <p>
+                            Conectando você a agricultores sustentáveis e produtos de qualidade, para uma vida melhor e transformando pequenos agricultores em amigos da vizinhança!
+                        </p>
+                    </Box>
+                    <Box className="btn-catalog">
+                        INICIE AGORA A SUA BUSCA
+                        <Box className="btn-arrow">
+                            <img src={Arrow} alt="" />
+                        </Box>
+                    </Box>
+                </Grid>
+
+                <Grid item xs={6} className="grid-item-2">
+                    <Box className="img-venda">
+                        <img src={FruitShop} alt="" className="img-config" />
                     </Box>
                 </Grid>
             </Grid>
-        </Box>
         </>
     )
 }
