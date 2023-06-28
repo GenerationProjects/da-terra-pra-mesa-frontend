d01990026da0e1b4adc7fa053102996b18e903aa
gaabrielarod <gabrielasouzarod@gmail.com>
2023-06-28 11:11:54 -0300

    criando cadastroProduto e deletarProduto

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import './App.css'
 import Navbar from './components/estaticos/navbar/Navbar'
 import Footer from './components/estaticos/footer/Footer'
 import Catalogo from './pages/catalogo/Catalogo'
+import CadastroProduto from './components/produtos/cadastroProduto/CadastroProduto'
+import DeletarProduto from './components/produtos/deletarProduto/DeletarProduto'
 
 export default function App() {
   return (
@@ -16,10 +18,20 @@ export default function App() {
         <div>
           <Routes>
             <Route path='/' element={<Home />} />
+
             <Route path='/cadastro' element={<Cadastro />} />
+
             <Route path='/login' element={<Login />} />
+
             <Route path='/sobre' element={<Sobre />} />
+
             <Route path='/catalogo' element={<Catalogo />} />
+
+            <Route path="/formularioProduto" element={<CadastroProduto />} />
+
+            <Route path="/formularioProduto/:id" element={<CadastroProduto />} />
+
+            <Route path="/deletarProduto/:id" element={<DeletarProduto />} />
           </Routes>
         </div>
         <Footer />
