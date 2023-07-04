97f17850e67c5d9b9b529cf696f78ebef4f2c083
Felipe Sousa <felipe.fps09@hotmail.com>
2023-07-04 17:05:01 -0300

    Pronto para deploy

--- src/services/service.ts
+++ src/services/service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://projeto-da-terra-pra-mesa.onrender.com"
+    baseURL: import.meta.env.VITE_API_URL
 })
 
 export const login = async (url: any, dados: any, setDado: any) => {
